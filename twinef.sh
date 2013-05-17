b21ce88f704204a66fc2e420beddb6d22da19455
Sebastien Caps <sebastien.caps@guardis.com>
2013-05-17 12:13:57 +0200

    Initial TWINEF commit

---
+++ twinef.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+##############
+# ViReR 2013 #
+# TWINEF     #
+# TriWire    #
+# Ignore     #
+# Not        #
+# Existing   #
+# Files      #
+##############
+IGNOREFILELIST=/tmp/tw_files2ignored
+touch $IGNOREFILELIST
+chmod 600 $IGNOREFILELIST
+
+# Get the last report file
+REPORTFILE=`ls -1rt /var/lib/tripwire/report/ | tail -1`
+
+# Change directory
+cd /etc/tripwire
+
+# Get the list of files to be ignored
+twprint -m r -r /var/lib/tripwire/report/$REPORTFILE | grep "File system error" -A 1 | awk '/Filename/ { print $2 }' > $IGNOREFILELIST
+
+# Backup twpol.txt
+[ ! -e /etc/tripwire/twpol.txt.BAK ] && cp /etc/tripwire/twpol.txt /etc/tripwire/twpol.txt.BAK
+
+# Modify the pol file
+for fil2ign in `cat $IGNOREFILELIST` ; do 
+        cat /etc/tripwire/twpol.txt | grep -v $fil2ign > /etc/tripwire/twpol.txt.1 
+        [ -e /etc/tripwire/twpol.txt.1 ] && rm -f /etc/tripwire/twpol.txt
+        mv /etc/tripwire/twpol.txt.1 /etc/tripwire/twpol.txt
+done
+
+# Sign new Pol file
+/usr/sbin/twadmin --create-polfile -S site.key /etc/tripwire/twpol.txt
+
+[ -e /var/lib/tripwire/$HOSTNAME.twd.BAK ] && rm -f /var/lib/tripwire/$HOSTNAME.twd.BAK
+mv /var/lib/tripwire/$HOSTNAME.twd /var/lib/tripwire/$HOSTNAME.twd.BAK
+
+# Now let's generate a new report
+/usr/sbin/tripwire --init
+
+# EOF
